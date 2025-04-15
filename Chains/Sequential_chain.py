8f3d2aa7688ced2788f30860e1c0e2c25122cc50
Tapoban Ray <raytapoban@gmail.com>
2025-04-16 02:44:51 +0530

    Learnt to work with Sequential Chains

--- Chains/Sequential_chain.py
+++ Chains/Sequential_chain.py
@@ -1,13 +1,12 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.sequential import SequentialChain
 from langchain_core.output_parsers import StrOutputParser
-from langchain.chains.llm import LLMChain
 from langchain_core.prompts import PromptTemplate
 from utils.my_enums import API_KEYS, LLM_MODELS
 
 llm = ChatGoogleGenerativeAI(
-    api_key=API_KEYS.GEMINI_API_KEY,
-    model=LLM_MODELS.GEMINI_MODEL,
+    api_key=API_KEYS.GEMINI_API_KEY.value,
+    model=LLM_MODELS.GEMINI_MODEL.value,
     temperature=0,
     max_retries=2,
 )
@@ -20,16 +19,9 @@ prompt2 = PromptTemplate.from_template(
     "Tell me the strategy of building a successful business in {industry}."
 )
 
-# chain1 = prompt | llm | StrOutputParser()
-# chain2 = prompt2 | llm | StrOutputParser()
+chain1 = prompt | llm | StrOutputParser()
+chain2 = prompt2 | llm | StrOutputParser()
 
-chain1 = LLMChain(llm=llm, prompt=prompt, output_key="company_name")
-chain2 = LLMChain(llm=llm, prompt=prompt2, output_key="strategy")
-
-final_chain = SequentialChain(
-    chains=[chain1, chain2],
-    input_variables=["Electronic Gadgets", "IOT"],
-    output_variables=["company_name", "strategy"],
-)
-ai_answer = final_chain.invoke(input={"product": "Electronic Gadgets", "industry": "IOT"})
+final_chain = chain1 | chain2
+ai_answer = final_chain.invoke({"product": "Electronic Gadgets", "industry": "IOT"})
 print(ai_answer)
