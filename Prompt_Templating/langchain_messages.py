9eaab17e3d957c1448d6f2cee9606d6a8aa73a54
Tapoban Ray <raytapoban@gmail.com>
2025-04-18 12:49:53 +0530

    Learnt to use Langchain_Messages and utilised it to create chat_history

---
+++ Prompt_Templating/langchain_messages.py
@@ -0,0 +1,26 @@
+# Simple Chatbot with ChatHistory
+
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from utils.my_enums import API_KEYS, LLM_MODELS
+
+
+llm = ChatGoogleGenerativeAI(
+    api_key=API_KEYS.GEMINI_API_KEY, model=LLM_MODELS.GEMINI_MODEL
+)
+
+chat_history = [
+    SystemMessage(content="You are a helpful assistent."),
+]
+
+while True:
+    user_prompt = input("User: ")
+    if (user_prompt.strip().lower()) in ["end", "exit"]:
+        break
+
+    chat_history.append(HumanMessage(content=user_prompt))
+
+    result = llm.invoke(chat_history)
+    print(f"Gemini: {result.content}\n")
+    chat_history.append(AIMessage(content=result.content))
+
