a2f9ac9face3f3d984fd83d4ee881c3714216c12
Tapoban Ray <raytapoban@gmail.com>
2025-05-16 11:38:08 +0530

    Learnt about CustomDocumentLoader

---
+++ Document_Loaders/custom_document_loader.py
@@ -0,0 +1,11 @@
+from langchain_core.document_loaders import BaseLoader
+
+class MyCustomDocumentLoader(BaseLoader):
+    """Here we can write our custom logic.
+    Add our custom load or lazy_load functions.
+    Finally, create our own DocumentLoader."""
+    pass
+
+
+# Learn more about DocumentLoader from Official Docs:
+# https://python.langchain.com/docs/concepts/document_loaders/
